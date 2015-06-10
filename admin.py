1845d3e67f56b8e545ec51c1d71de5968d7a4045
tim <tim@Narwhal.stny.rr.com>
2015-06-09 20:15:33 -0400

    change database schema, replace long charfield with textfield

--- admin.py
+++ admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Event, Notice, Fellowship, FellowshipMessage
 
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields' : ('event_time','location','title')}),
+            (None, {'fields' : ('desc',), 'classes' : ('wide', 'extrapretty',)}),
+            (None, {'fields' : ('pdf',)}),
+            ]
+
+    list_display = ('event_time', 'location', 'title', 'owner', 'pub_time')
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+
 admin.site.register(Notice)
 admin.site.register(Fellowship)
 admin.site.register(FellowshipMessage)
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Notice(models.Model):
     #outdated event won't be displayed according to event_time
     event_time = models.DateTimeField('Event Time')
-    desc = models.CharField('Notice Content', max_length=500)
+    desc = models.TextField('Notice Content')
     pub_time = models.DateTimeField('Time Published', auto_now_add=True)
     owner = models.ForeignKey(User, limit_choices_to={'is_staff': True}, verbose_name="Publisher")
 
@@ -13,27 +13,27 @@ class Event(models.Model):
     #outdated event won't be displayed according to event_time
     event_time = models.DateTimeField('Event Time')
     pub_time = models.DateTimeField('Time Published', auto_now_add=True)
-    location = models.CharField('Location', max_length=150)
-    title = models.CharField('Event Summary', max_length=150)
-    desc = models.CharField('Event Description', max_length=800)
+    location = models.CharField('Location', max_length=50)
+    title = models.CharField('Event Summary', max_length=50)
+    desc = models.TextField('Event Description')
     owner = models.ForeignKey(User, verbose_name="Publisher")
-    pdf = models.FileField(verbose_name = "Flyer", upload_to='pdfs')
+    pdf = models.FileField(verbose_name = "Flyer", upload_to='pdfs', null=True, blank=True)
 
 class Fellowship(models.Model):
 
-    name = models.CharField('Fellowship Name', max_length=100)
-    desc = models.CharField('Fellowship Description', max_length=1000)
-    location = models.CharField('Location', max_length=100)
+    name = models.CharField('Fellowship Name', max_length=50)
+    desc = models.TextField('Fellowship Description')
+    location = models.CharField('Location', max_length=50)
 
     dp_order = models.IntegerField('Display Order')
 
     #contact info
-    admin = models.CharField('Person in Charge', max_length=30)
+    admin = models.CharField('Person in Charge', max_length=20)
     admin_email = models.EmailField('Email')
     admin_phone = models.CharField('Phone', max_length=16, blank = True)
 
 class FellowshipMessage(models.Model):
-    msg = models.CharField('Message', max_length=150)
+    msg = models.TextField('Message')
     pub_time = models.DateTimeField('Time Published', auto_now_add=True)
     fellowship = models.ForeignKey(Fellowship, verbose_name='Fellowship')
     owner = models.ForeignKey(User, verbose_name="Publisher")
